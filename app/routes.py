ced98fff0b30b9fd2c7f8e50603defbc7def6c4c
Aditya Wadaskar <wadaskar.aditya@gmail.com>
2020-01-22 23:49:34 -0800

    Add routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
 from app import app
+from app.models import Student, Project, Sponsor
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/projects')
+def projects():
+    project = Project.query.all()
+    return "Project"
+
+@app.route('/sponsors')
+def sponsors():
+    pass
+
+@app.route('/students')
+def students():
+    pass
