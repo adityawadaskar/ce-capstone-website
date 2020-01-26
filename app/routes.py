cc59ea6f908585aff9a11cdece0438816fec15ae
Aditya Wadaskar <wadaskar.aditya@gmail.com>
2020-01-25 21:56:49 -0800

    Add sliding logos and customize home page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect
 from app.models import Student, Project, Sponsor
 
 @app.route('/')
@@ -10,26 +10,27 @@ def index():
 
 @app.route('/schedule')
 def schedule():
-    return render_template('index.html')
+    return render_template('schedule.html')
 
 @app.route('/projects')
 def projects():
     project = Project.query.all()
-    return render_template('index.html')
+    return render_template('projects.html')
 
 @app.route('/resources')
 def resources():
-    return render_template('index.html')
+    return render_template('resources.html')
 
 @app.route('/sponsors')
 def sponsors():
-    return render_template('index.html')
+    return render_template('sponsors.html')
 
 @app.route('/students')
 def students():
     students = Student.query.all()
-    return render_template('index.html')
+    return render_template('students.html')
 
 @app.route('/capstoneday')
+@app.route('/capstone-day')
 def capstone_day():
-    return render_template('index.html')
+    return redirect("https://www.ce.ucsb.edu/undergrad/curriculum/capstone/events/ece189", code=302)
