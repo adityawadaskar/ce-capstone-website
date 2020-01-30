be741e8fce11841621085890fb6de54c0680cc8d
Aditya Wadaskar <wadaskar.aditya@gmail.com>
2020-01-30 14:43:50 -0800

    Cleanup up folder structure

--- app/build/static/img/sponsors/format_images.sh
+++ app/build/static/img/sponsors/format_images.sh
@@ -1,15 +1,6 @@
-mkdir current_sponsors_tmp
-mogrify -format png -path ./current_sponsors_tmp ./current_sponsors/*
-rm -r current_sponsors
-mv current_sponsors_tmp current_sponsors
-mogrify -resize 100x100 -background white -gravity center -extent 100x100 -path ./current_sponsors_resized ./current_sponsors/*.png
-mogrify -resize 500x200 -background white -gravity center -extent 500x200 -path ./current_sponsors_resized_large ./current_sponsors/*.png
-
-mkdir all_sponsors_tmp
-mogrify -format png -path ./all_sponsors_tmp ./all_sponsors/*
-rm -r all_sponsors
-mv all_sponsors_tmp all_sponsors
-mogrify -resize 100x100 -background white -gravity center -extent 100x100 -path ./all_sponsors_resized ./all_sponsors/*.png
-
-cp -R current_sponsors/. all_sponsors/.
-cp -R current_sponsors_resized/. all_sponsors_resized/.
+mkdir sponsors_tmp
+mogrify -format png -path ./sponsors_tmp ./sponsors_original/*
+rm -r sponsors_original
+mv sponsors_tmp sponsors_original
+mogrify -resize 100x100 -background white -gravity center -extent 100x100 -path ./sponsors_small ./sponsors_original/*.png
+mogrify -resize 500x200 -background white -gravity center -extent 500x200 -path ./sponsors_large ./sponsors_original/*.png
